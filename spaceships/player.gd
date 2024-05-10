7b979c4fc942c41fffb385c99cefee146eda80ea
BH3Panzer <clementcattin74@gmail.com>
2024-05-10 21:20:08 +0200

    completed parallax background, added player and basic movement

---
+++ spaceships/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var forward_speed = 100
+
+func _ready():
+	$AnimationPlayer.play("idle")
+func _physics_process(delta):
+	position.y -= forward_speed * delta
+	if Input.is_action_pressed("right"):
+		position.x += SPEED * delta
+	if Input.is_action_pressed("left"):
+		position.x -= SPEED * delta
+	if Input.is_action_pressed("up"):
+		position.y -= SPEED * delta
+	if Input.is_action_pressed("down"):
+		position.y += SPEED * delta
