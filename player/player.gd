4260bec82b289ffea3be3d27c754dcd1e87b0817
BH3Panzer <clementcattin74@gmail.com>
2024-05-11 11:40:48 +0200

    added basic bullet to shoot

--- player/player.gd
+++ player/player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 var forward_speed = 100
-
+@export var Bullet : PackedScene
+@onready var timer = $Timer
+var is_shooting := false
 func _ready():
 	$AnimationPlayer.play("idle")
+	is_shooting = false
+
 func _physics_process(delta):
 	position.y -= forward_speed * delta
 	if Input.is_action_pressed("right"):
@@ -16,7 +20,29 @@ func _physics_process(delta):
 		position.y -= SPEED * delta
 	if Input.is_action_pressed("down"):
 		position.y += SPEED * delta
+
 	if global_position.x >= 853/2 - 16 :
 		position.x -= SPEED * delta
 	if global_position.x <= -853/2 + 16 :
 		position.x += SPEED * delta
+
+func _input(event):
+	if event.is_action_pressed("shoot") && timer.is_stopped():
+		is_shooting = true
+		shoot()
+		timer.start()
+	elif event.is_action_released("shoot"):
+		timer.stop()
+		is_shooting = false
+
+
+func _on_timer_timeout():
+	shoot()
+	is_shooting = false
+
+func shoot():
+	var b = Bullet.instantiate()
+	owner.add_child(b)
+	b.transform = $FrontCanon.global_transform
+	b.scale.x = 1
+	b.scale.y = 1
