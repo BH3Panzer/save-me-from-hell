d0d6a36dd280e78602b57871df0b86c95f002c01
raphael.georget <raphael.georget@TLPU002.ETAB.LOCAL>
2024-05-14 12:58:11 +0200

    ajout tourelle simple

---
+++ ennemi/double_turret.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+@export var Bullet : PackedScene
+@onready var timer = $Timer
+
+func _ready():
+	timer.start()
+
+func _on_timer_timeout():
+	shoot()
+	timer.start()
+
+func shoot():
+	for i in range(2):
+		var b = Bullet.instantiate()
+		owner.add_child(b)
+		b.transform = $FrontCanon.global_transform
+		b.scale.x = 1
+		b.scale.y = 1
--- ennemi/ennemi_bullet.gd
+++ ennemi/ennemi_bullet.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-var speed = 900
+var speed = 150
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
 func _on_body_entered(body):
-	if body.is_in_group("Enemies"):
+	if body.is_in_group("Player"):
 		body.queue_free()
 	queue_free()
