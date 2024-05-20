a4587e117f3d6576c34b5e439cd2be8f9b0c6925
Fraii <raphael.georget01@gmail.com>
2024-05-20 15:58:40 +0200

    un c'est déjà bien

--- levels/set_ennemis.gd
+++ levels/set_ennemis.gd
@@ -8,18 +8,57 @@ extends Node2D
 
 #[name, pos_x, pos_y, type of turret, rotation, rotation direction (if type==rotating)]
 var allEnnemies = [
-	["simple_turret", -75, -375, "rotating", 0, -1],
-	["simple_turret", 75, -375, "normal", -90, null],
-	["shotgun_turret", -300, -800, "rotating", 0, 1],
-	["shotgun_turret", 300, -800, "normal", 0, null],
-	["simple_turret", -350, -1500, "rotating", 0, 1],
-	["simple_turret", -250, -1500, "normal", 0, null],
-	["double_turret", -150, -1500, "rotating", 0, -1],
-	["double_turret", -50, -1500, "normal", 0, null],
-	["triple_turret", 50, -1500, "rotating", 0, 1],
-	["triple_turret", 150, -1500, "normal", 0, null],
-	["quadruple_turret", 250, -1500, "rotating", 0, -1],
-	["quadruple_turret", 350, -1500, "normal", 0, null]
+	["simple_turret", -200, -375, "normal", -45, null],
+	["simple_turret", 200, -375, "normal", 45, null],
+	["double_turret", 0, -500, "rotating", 0, 1],
+	
+	["simple_turret", -400, -700, "rotating", -20, 1],
+	["simple_turret", -200, -800, "rotating", 180, -1],
+	["simple_turret", 200, -800, "rotating", 180, -1],
+	["simple_turret", 400, -700, "rotating", 20, 1],
+	
+	["shotgun_turret", 0, -1100, "normal", 0, null],
+	["simple_turret", -100, -1120, "normal", 10, null],
+	["simple_turret", 100, -1120, "normal", -10, null],
+	["simple_turret", -150, -1140, "normal", 30, null],
+	["simple_turret", 150, -1140, "normal", -30, null],
+	
+	["simple_turret", -400, -1300, "normal", 0, null],
+	["simple_turret", -360, -1300, "normal", -45, null],
+	["simple_turret", -360, -1340, "normal", -90, null],
+	["simple_turret", 400, -1300, "normal", 0, null],
+	["simple_turret", 360, -1300, "normal", 45, null],
+	["simple_turret", 360, -1340, "normal", 90, null],
+	["simple_turret", -20, -1380, "rotating", -45, 1],
+	["simple_turret", 20, -1380, "rotating", 45, -1],
+	
+	["quadruple_turret", -200, -1700, "rotating", -45, 1],
+	["quadruple_turret", 200, -1700, "rotating", 45, -1],
+	
+	["double_turret", -300, -1900, "normal", 90, null],
+	["double_turret", -270, -1900, "normal", 90, null],
+	["double_turret", -100, -1900, "normal", 90, null],
+	["double_turret", -70, -1900, "normal", 90, null],
+	["double_turret", 70, -1900, "normal", 90, null],
+	["double_turret", 100, -1900, "normal", 90, null],
+	["double_turret", 270, -1900, "normal", 90, null],
+	["double_turret", 300, -1900, "normal", 90, null],
+	
+	["triple_turret", -300, -2300, "normal", -75, null],
+	["triple_turret", 300, -2300, "normal", 75, null],
+	["shotgun_turret", 0, -2400, "normal", 0, null],
+	
+	["shotgun_turret", -400, -2600, "normal", -30, null],
+	["shotgun_turret", 400, -2600, "normal", 30, null],
+	
+	["double_turret", -310, -2900, "rotating", 90, 1],
+	["double_turret", -270, 2900, "rotating", 90, -1],
+	["double_turret", -110, -2900, "rotating", 90, 1],
+	["double_turret", -70, -2900, "rotating", 90, -1],
+	["double_turret", 70, -2900, "rotating", 90, 1],
+	["double_turret", 110, -2900, "rotating", 90, -1],
+	["double_turret", 270, -2900, "rotating", 90, 1],
+	["double_turret", 310, -2900, "rotating", 90, -1]
 	]
 
 func _ready():
